5a8e1272139b20bb1c437c6973d4ff24a808fa73
LouisK112 <Louis.Krueger@web.de>
2016-02-12 23:33:13 +0100

    6 Gelöscht und 10 hinzugefügt

--- TestGitHubUebung10/src/village/SomeNewClass.java
+++ TestGitHubUebung10/src/village/SomeNewClass.java
@@ -7,11 +7,12 @@ public class SomeNewClass {
 		
 		System.out.print("Das hier ist neu4!");
 		System.out.print("Das hier ist neu5!");
-		System.out.print("Das hier ist neu6!");
+		
 		System.out.print("Das hier ist neu7!");
 
 		System.out.print("Das hier ist neu8!");
 		System.out.print("Das hier ist neu9!");
+		System.out.print("Das hier ist neu10!");
 	}
 
 }

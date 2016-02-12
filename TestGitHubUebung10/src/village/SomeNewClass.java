4d3b2750985fc401e3b39356fbf39c0ff1598310
LouisK112 <Louis.Krueger@web.de>
2016-02-12 23:31:51 +0100

    3 gelöscht und 9 Hinzugefügt

--- TestGitHubUebung10/src/village/SomeNewClass.java
+++ TestGitHubUebung10/src/village/SomeNewClass.java
@@ -4,12 +4,14 @@ public class SomeNewClass {
 	
 	public SomeNewClass() {
 		System.out.print("Das hier ist neu!");
-		System.out.print("Das hier ist neu3!");
+		
 		System.out.print("Das hier ist neu4!");
 		System.out.print("Das hier ist neu5!");
 		System.out.print("Das hier ist neu6!");
 		System.out.print("Das hier ist neu7!");
+
 		System.out.print("Das hier ist neu8!");
+		System.out.print("Das hier ist neu9!");
 	}
 
 }

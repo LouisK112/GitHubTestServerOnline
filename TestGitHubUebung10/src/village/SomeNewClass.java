c19cb2ad51ea4978187ad7e7dec7dcb265ec5b32
LouisK112 <Louis.Krueger@web.de>
2016-02-12 23:20:48 +0100

    SomeNewClass 6+7 Ausgabe

--- TestGitHubUebung10/src/village/SomeNewClass.java
+++ TestGitHubUebung10/src/village/SomeNewClass.java
@@ -7,6 +7,8 @@ public class SomeNewClass {
 		System.out.print("Das hier ist neu3!");
 		System.out.print("Das hier ist neu4!");
 		System.out.print("Das hier ist neu5!");
+		System.out.print("Das hier ist neu6!");
+		System.out.print("Das hier ist neu7!");
 	}
 
 }

29ec36143ad576ae72d5692d1a2349dc5cd42699
LouisK112 <Louis.Krueger@web.de>
2016-02-13 11:01:59 +0100

    merge rückgägngig

--- TestGitHubUebung10/src/village/SomeNewClass.java
+++ TestGitHubUebung10/src/village/SomeNewClass.java
@@ -13,24 +13,3 @@ public class SomeNewClass {
 	}
 
 }
-=======
-package village;
-
-public class SomeNewClass {
-	
-	public SomeNewClass() {
-		
-		
-		System.out.print("Das hier ist neu4!");
-		System.out.print("Das hier ist neu5!");
-		System.out.print("Das hier ist neu6!");
-		System.out.print("Das hier ist neu7!");
-
-		System.out.print("Das hier ist neu8!");
-		System.out.print("Das hier ist neu9!");
-		System.out.print("Das hier ist neu10!");
-		System.out.print("Das hier ist neu11!");
-		System.out.print("Das hier ist neu12!");
-	}
-
-
